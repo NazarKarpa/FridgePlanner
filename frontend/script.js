5ecf218d98bcc22a097ecfe5351637c556af2671
sem_naz <semerenkonazar90@gmail.com>
2026-07-06 20:29:10 +0300

    add API, connect

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("JS працює");
 
-    const productInput = document.querySelector(".productInput")
-    const buttonSearch = document.querySelector(".button-search")
-    
-    
+
+    const productInput = document.getElementById("productInput");
+    const searchBtn = document.querySelector(".button-search");
+
+
+        //-----------------API---------------//
+
+    const API_KEY = "d4d566d7a8e44533b61dd0506782f20e";
+
+    searchBtn.addEventListener("click", async () => {
+
+        const ingredients = productInput.value;
+
+        const response = await fetch(
+            `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=5&apiKey=${API_KEY}`
+        );
+
+        const recipes = await response.json();
+
+        console.log(recipes);
+    });
 
 });
